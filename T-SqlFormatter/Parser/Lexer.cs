e7354c4c956e0a458702f9903b1f43652a053087
Andrew Harfst <andrewharfst@hotmail.com>
2023-12-16 14:55:34 -0500

    Add project files.

---
+++ T-SqlFormatter/Parser/Lexer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_SqlFormatter.Parser
+{
+    public class Lexer
+    {
+
+        public void nextToken()
+        {
+            char nextChar = charReader.peek();
+            switch (nextChar)
+            {
+                case "\"":
+                    return processStringLiteral();
+                case "0":
+                case "1":
+                case "2":
+                    ...
+            case "9":
+                    return processNumericLiteral();
+            }
+        }
+
+    }
+}
